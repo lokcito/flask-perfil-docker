8d5bd8de25ef5d39a56b3cb678825cb59329c22c
Ray Rojas <desarrollador06@upsjb.edu.pe>
2025-02-20 17:43:31 -0500

    fix app.py

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-DEBUG = os.environ["DEBUG"] == "TRUE"
+DEBUG = os.environ.get("DEBUG", "TRUE") == "TRUE"
 
 @app.route("/")
 def home():
     return render_template('index.html', title='Equisd')
 
 if __name__ == "__main__":
+    print(f"running debug as {DEBUG}")
     app.run(debug=DEBUG)
