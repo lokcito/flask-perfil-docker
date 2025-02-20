cbe7e22469789669c3adfd7d5c8443b8f8e65b6d
Ray Rojas <desarrollador06@upsjb.edu.pe>
2025-02-20 15:13:13 -0500

    simple app with docker

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+DEBUG = os.environ["DEBUG"] == "TRUE"
+
+@app.route("/")
+def home():
+    return render_template('index.html', title='Equisd')
+
+if __name__ == "__main__":
+    app.run(debug=DEBUG)
